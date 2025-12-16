5783c074a89949cae0bb808d7442104581b8bee4
1ethon0822 <1ethyjj9240@gmail.com>
2025-12-16 14:43:51 +0900

    수수수수업

---
+++ js/ch18/data_type.js
@@ -0,0 +1,35 @@
+/*
+자료형(데이터 유형)
+-비교연사자의 결과 타입 boolean : 참(true), 거짓(false)
+
+*/
+let bool1 = true;
+let bool2 = false;
+
+console.log('bool1:', bool1);
+console.log('bool2:', bool2);
+
+console.log('typeof bool1:', typeof bool1);
+console.log('typeof bool1:', typeof (bool1));
+
+/*
+    불린타입은 비교 연산자의 결과로 많이 사용된다.
+*/
+let n1 = 10, n2 = 20;
+bool1 = n1 > n2;
+// >는 비교연산자
+console.log('bool1:', bool1);
+console.log('n1 > 10:', n1 > 10);
+console.log('n1 >=10:', n1 >= 10);
+console.log('-------------------');
+console.log('n2 > 20:', n2 > 20);
+console.log('n2 <= 20:', n2 <= 20);
+console.log('-------------------');
+console.log('n1 == n2:', n1 == n2);
+ // == 값이 같으면 t, 다르면 f
+console.log('n1 != n2:', n1 != n2);
+// != 값이 같으면 f, 다르면 t
+console.log('----------------논리연사자');
+console.log('!(n1 == n2):', !(n1 == n2));
+console.log('!(n1 != n2:)', !(n1 != n2));
+//!가 들어가면 반대로 생각(not)
--- js/ch18/deta_type.js
+++
@@ -1,9 +0,0 @@
-/*
-자료형(데이터 유형)
-- boolean : 참(true), 거짓(false)
-
-*/
-let bool1 = true;
-let bool2 = false;
-
-console.log('bool1:', bool1);
