92a03fb74dfd1b2e031a6b6287476e0c52eca20c
1ethon0822 <1ethyjj9240@gmail.com>
2025-12-17 11:41:00 +0900

    으으으

--- js/ch18/object2.js
+++ js/ch18/object2.js
@@ -1,6 +1,6 @@
 /* object2.js
-{}는  객체를 열고 닫고 하는 문법이다라는 의미
-
+{}는 익명객체(이름이 없는). 객체를 열고 닫고 하는 문법(문서)이다라는 의미
+같은이름을 전체 한번에 바꾸고 싶으면 바꾸고 싶은 문자클릭-> 우측키-> symbol or ctrl+f 누르고 변경
 */
 const person1 = {
     name : '김철수',
@@ -8,6 +8,23 @@ const person1 = {
     married: false
 };
 
+console.log(typeof person1);
 console.log(person1);
 
-// 
+//person1이 가리키고 있는 객체의 married속성값을 true로 수정하시오
+
+console.log(person1); 
+
+person1.married = true;
+console.log(person1);
+
+person1['age'] = 30;
+const key = 'name';
+person1[key] = '권수영';
+//person1['name'] = '권수영'; // 이렇게 작성한 것과 같다.
+console.log(person1);
+
+//자바스크립트는 객체 생성이후에 속성추가가 가능하다.(동적할당)
+person1.address = '대구';
+person1['height'] = 174.3;
+console.log(person1);
