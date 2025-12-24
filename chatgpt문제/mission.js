47b5b4079d192baa3aea2cf84cc6cf0d246b6a0f
1ethon0822 <1ethyjj9240@gmail.com>
2025-12-24 14:51:07 +0900

    ch04

---
+++ chatgpt문제/mission.js
@@ -0,0 +1,12 @@
+const products = [
+    { name: "노트북", price:  1200000, inStock: true },
+    { name: "마우스", price: 30000, inStock: false },
+    { name: "키보드", price: 80000, inStock: true },
+    { name: "모니터", price: 250000, inStock: true },
+    { name: "USB", price: 15000, inStock: false }
+];
+
+const arr1= products.filter(products => inStock===0)
+console.log(arr1);
+
+const arr2 = products.map(item => )
